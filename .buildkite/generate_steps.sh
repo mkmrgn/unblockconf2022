8e8be3dc31c7fbe642b733644a2dc18e3ba8216f
mkmrgn <mike@morgan.ms>
2022-12-04 12:43:58 -0800

    more script fixes

--- .buildkite/generate_steps.sh
+++ .buildkite/generate_steps.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+decision_steps=$(cat <<EOF
+steps:
+  - block: "What now?"
+    prompt: "Choose the next set of steps to be dynamically generated"
+    fields:
+      - select: "Choices"
+        key: "choice"
+        options:
+          - label: "Display the UnblockConf logo again"
+            value: "logo"
+          - label: "Print 'hello world' a bunch of times in parallel"
+            value: "hello-world"
+          - label: "Finish the build green"
+            value: "build-pass"
+          - label: "Finish the build red"
+            value: "build-fail"
+  - label: "Process input"
+    command: ".buildkite/generate_steps.sh"
+EOF
+
 # this bit is an ugly hack to avoid checking metadata on first run of the script
 current_state=""
 first_step_label=":pipeline: Upload Pipeline"
 if [ "$BUILDKITE_LABEL" != "$first_step_label" ]; then
   current_state=$(buildkite-agent meta-data get "choice")
+else
+  printf "$decision_steps"
+  exit 0
 fi
 
 case $current_state in
@@ -38,22 +61,4 @@ EOF
   ;;
 esac
 
-buildkite-agent pipeline upload <<EOF
-steps:
-  - block: "What now?"
-    prompt: "Choose the next set of steps to be dynamically generated"
-    fields:
-      - select: "Choices"
-        key: "choice"
-        options:
-          - label: "Display the UnblockConf logo again"
-            value: "logo"
-          - label: "Print 'hello world' a bunch of times in parallel"
-            value: "hello-world"
-          - label: "Finish the build green"
-            value: "build-pass"
-          - label: "Finish the build red"
-            value: "build-fail"
-  - label: "Process input"
-    command: ".buildkite/generate_steps.sh"
-EOF
+printf "decision_steps" | buildkite-agent pipeline upload
