2fb1b7e9e06d895a886604a6dc119df135f8288b
Mike Morgan <mike@morgan.ms>
2022-12-06 07:41:40 -0800

    Update step_annotate.sh

--- step_annotate.sh
+++ step_annotate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "This is a link to step $BUILDKITE_PARALLEL_JOB - [Step $BUILDKITE_PARALLEL_INDEX]($BUILDKITE_BUILD_URL#$BUILDKITE_JOB_ID)\n" | buildkite-agent annotate --append
+printf "This is a link to step $BUILDKITE_PARALLEL_JOB - [Step $BUILDKITE_PARALLEL_INDEX]($BUILDKITE_BUILD_URL#$BUILDKITE_JOB_ID)\n\n" | buildkite-agent annotate --append
